9a52deee45d3f0abe801e0c49b35887766205310
xuanthienl <xuanthien14031998qt@gmail.com>
2023-10-08 02:04:33 +0700

    Upload

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import jwt
 from flask import Flask, jsonify, request, abort
 
 
-JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
+JWT_SECRET = os.environ.get('JWT_SECRET')
+LOG_LEVEL = os.environ.get('LOG_LEVEL')
 
 
 def _logger():
